378bb398138b1d4b0f57dcaca00c5bd177bbdb80
lia <lia.bfs@aluno.ifsc.edu.br>
2025-12-04 16:52:00 -0300

    coisas

---
+++ cadastro.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="pt" dir="ltr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.rtl.min.css">
+
+    <title>Cadastro Floricultura</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Cadastro</h1><br><br>
+                <form action="cadastro_script.php" method="POST">
+                    <div class="form-group">
+                        <label for="nome">Informe o nome da planta:</label>
+                        <input type="text" class="form-control" placeholder="Ex. 'Orquídea'" name ="nome" required><br>
+                        
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nomeCientifico">Informe o nome  científico da planta:</label>
+                        <input type="text" class="form-control" placeholder="Ex. 'Orchidaceae'" name ="nomeCientifico" required><br>
+                        
+                    </div>
+
+                    <div class="form-group">
+                        <label for="quantidade">Informe a quantidade de sementes em estoque:</label>
+                        <input type="text" class="form-control" placeholder="Quantidade de sementes" name ="quantidade" required><br>
+                        
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-success">
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+  <!-- Optional JavaScript-->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/
+  X+965Dz00rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="
+  sha384-U02eT@CpHqdSJQ6hJty5KVphtPhzWj9W01c1HTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="
+  sha384-3j5mVgyd@p3pXB1rRibZUAYOIIy60rQ6VrjIEaFf/nJGzIxFDsf4x0xIM+807jRM" crossorigin="anonymous"></script>
+  </body>
+  </html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pt" dir="ltr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.rtl.min.css">
+
+    <title>Cadastro Floricultura</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+               <div class="jumbotron">
+              <h1 class="display-4">Hello, world!</h1>
+              <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or
+              information.</p>
+              <hr class="my-4">
+              <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+              <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+        </div>
+    </div>
+
+  <!-- Optional JavaScript-->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/
+  X+965Dz00rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="
+  sha384-U02eT@CpHqdSJQ6hJty5KVphtPhzWj9W01c1HTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="
+  sha384-3j5mVgyd@p3pXB1rRibZUAYOIIy60rQ6VrjIEaFf/nJGzIxFDsf4x0xIM+807jRM" crossorigin="anonymous"></script>
+  </body>
+  </html>
