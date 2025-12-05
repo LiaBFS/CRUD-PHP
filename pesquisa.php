c17f32d0bce111f0fd770c0e48e254233187df96
Your Name <lia.bfs@aluno.ifsc.edu.br>
2025-12-05 09:33:07 -0300

    tabela na pesquisa

--- pesquisa.php
+++ pesquisa.php
@@ -16,6 +16,46 @@
         <div class="row">
             <div class="col">
                 <h1>Procure Plantas</h1><br><br>
+
+                <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+                </nav><br>
+
+                <table class="table table-hover table-success table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>@social</td>
+                        </tr>
+                    </tbody>
+                    </table>
                 
 
                 <a href="index.php" class="btn btn-primary">Voltar</a>
